46120644bc1405ccc69ba763bd48a4956f25286b
Thomas Brus <thomas.brus@me.com>
2012-11-22 10:48:49 +0100

    Implemented calculating pi with threads

--- assignment-1/task-1/pi.c
+++ assignment-1/task-1/pi.c
@@ -1,20 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <pthread.h>
 
-long double calculate(int max_iterations) {
-  int iteration = 0;
-  long double pi = 0;
+typedef struct {
+  int max_iterations, max_time;
+} boundaries;
 
-  while (iteration < max_iterations) {
-    pi = pi + (pow(-1, iteration) / (2 * iteration + 1));
-    iteration++;
+long double pi = 0;
+int done;
+
+void *calculate(void *args) {
+  int i = 0;
+  int time_elapsed = 0;
+  boundaries *thread_boundaries = args;
+
+  int begin = (int)time(NULL);
+  time_t timer;
+  
+  while ((i < thread_boundaries->max_iterations) && (time_elapsed < thread_boundaries->max_time)) {
+
+    pi = pi + (pow(-1, i) / (2 * i + 1));
+    
+    timer = time(NULL);
+    time_elapsed = ((int)timer - begin);
+
+    i++;
   }
 
-  return 4 * pi;
+  done = 1;
+  return NULL;
 }
 
-int main() {
-  long double pi = calculate(10000);
-  printf("The value of π ≈ %.40Le\n", pi);
+int main(int argc, char* argv[]) {
+  int max_time, max_iterations;
+
+  boundaries* thread_boundaries;
+  thread_boundaries = malloc(sizeof(boundaries));
+
+  thread_boundaries->max_time = atoi(argv[1]);
+  thread_boundaries->max_iterations = atoi(argv[2]);
+
+  pthread_t pi_thread;
+  pthread_create(&pi_thread, NULL, calculate, thread_boundaries);
+
+  int i = 0;
+  while (done != 1) {
+    usleep(100000);
+    printf("The value of π ≈ %.40Le\n", pi * 4);
+    i++;
+  }
+
+  pthread_join(pi_thread, NULL);
+
+  return 0;
 }
