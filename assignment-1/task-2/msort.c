8da341084fab8e6356707f8de8a326f6ddd1949f
wietze <wietzuh@gmail.com>
2012-11-28 15:28:37 +0100

    Minor fixes (typos, variable names)

--- assignment-1/task-2/msort.c
+++ assignment-1/task-2/msort.c
@@ -10,44 +10,45 @@ task_t** splitTasks(task_t** tasks, int start, int count){
 
   // Generate sublist
   for (i = 0; i < count; i++) {
-  result[i] = tasks[start + i];       
+    result[i] = tasks[start + i];       
   }
   return result;
 }
 
-void merge(task_t** tasks, task_t** tasksleft, int n1,task_t** tasksright, int n2){
-  int p,p1,p2;
-  p=p1=p2 =0;
+void merge(task_t** tasks, task_t** tasksleft, int size_left,task_t** tasksright, int size_right){
+  // Initialize index pointers for tasks, tasksleft and tasksright
+  int p, p1, p2;
+  p = p1 = p2 = 0;
 
-  while (p1 < n1 && p2 < n2) { 
-    // Check whether the indexpointers p1 and p2 are stil large enough
+  while (p1 < size_left && p2 < size_right) { 
+    // Check whether the index pointers p1 and p2 are stil large enough
     if (tasksleft[p1]->id < tasksright[p2]->id) { 
       // The smallest element in left is smaller than the smallest element in right
       tasks[p++] = tasksleft[p1++];
     } else {
-      //The smallest element in right is smaller than the smallest element in left*/
+      // The smallest element in right is smaller than the smallest element in left
       tasks[p++] = tasksright[p2++];
     }
   }
-  while (p1 < n1) { 
-    // The right one is empty; now add evertything of the left
+  while (p1 < size_left) { 
+    // The right one is empty; now add everything that is left in the left one
     tasks[p++] = tasksleft[p1++];
   }
 
-  while (p2 < n2) { 
-    // The left one is empty; now add evertything of the right
+  while (p2 < size_right) { 
+    // The left one is empty; now add everything that is left in the right one
     tasks[p++] = tasksright[p2++]; 
   }        
 }
 
 void msort(task_t** tasks, int count){
-  // Base case: 0 elements or 1 element are already 'sorted'
+  // Base case: 0 or 1 element are already 'sorted'
   if(count<=1){
     return;	
   }
 
   // Divide input list length in two parts
-  int size_left = count/2;
+  int size_left = count / 2;
   int size_right = count - size_left;
 	
   // Make sublist with these sizes
