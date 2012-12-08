27b203f517c8e738535a47fafae2432417a21c74
Thomas Brus <thomas.brus@me.com>
2012-12-08 15:40:10 +0100

    Added task.c (assignment 2)

--- assignment-2/task-1/task.c
+++ assignment-2/task-1/task.c
@@ -1,9 +1,22 @@
+
+// includes ...
+
+typedef struct {
+  int size;
+  linkedListElement* first;
+} linkedList;
+
+
+typedef struct {
+  linkedListElement* next;
+  void* task;
+
+} linkedListElement;
+
 void *task_alloc(void) {
   
-
 }
 
 void task_free(void *ptr) {
-
-
+    
 }
