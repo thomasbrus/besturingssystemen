44a1313868364369f27332b3f700e27b0fe6cc33
Thomas Brus <thomas.brus@me.com>
2012-12-08 17:49:08 +0100

    First test passes

--- assignment-2/task-1/task.c
+++ assignment-2/task-1/task.c
@@ -1,22 +1,21 @@
+#include "as2_t1.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-// includes ...
-
-typedef struct {
-  int size;
-  linkedListElement* first;
-} linkedList;
-
-
-typedef struct {
-  linkedListElement* next;
-  void* task;
-
-} linkedListElement;
+unsigned long memory_address;
 
 void *task_alloc(void) {
-  
+
+  if (memory_address) {
+    /* Increase the pointer by the size of one task_t */
+    memory_address += sizeof(task_t);
+  } else {
+    /* Set memory pointer to the start of the block */
+    memory_address = (unsigned long) get_MEM_BLOCK_START();
+  }
+
+  return (void *)memory_address;
 }
 
 void task_free(void *ptr) {
-    
 }
