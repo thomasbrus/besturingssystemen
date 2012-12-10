cc1628e8ffa82dacf35d827d65925fc6941aa5ab
Thomas Brus <thomas.brus@me.com>
2012-12-10 13:34:00 +0100

    Solved !11

--- assignment-2/task-1/task.c
+++ assignment-2/task-1/task.c
@@ -1,42 +1,57 @@
 #include "as2_t1.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int *memory_slots[MAX_TASK_COUNT] = { 0 };
+/* Reserve the first halve of the available memory for memory management */
+int memory_management_offset = 524288;
+int memory_slot_count = MAX_TASK_COUNT / 2;
 
 void *task_alloc(void) {
-  unsigned long address;
+  unsigned long pointer;
 
   /* Find the first free slot */
-  int slot = 0;
-  while (slot <= MAX_TASK_COUNT && (int)memory_slots[slot] == 1) {
-    slot++;
+  int slot_array_index = 0;
+  
+  int slot_array_location;
+  int *slot_array_pointer;
+
+  while (slot_array_index <= memory_slot_count) {
+    slot_array_location = (unsigned long)get_MEM_BLOCK_START() + sizeof(int) * slot_array_index;    
+    slot_array_pointer = (int *)slot_array_location;    
+    if (*slot_array_pointer == 0) break;
+    slot_array_index++;
   }
 
-  if (slot < MAX_TASK_COUNT) {
-    /* Mark this memory block as taken */
-    memory_slots[slot] = (int *)1;
+  if (slot_array_index < memory_slot_count) {
+    /* Mark this memory block as taken */    
+    /* memset((void *)slot_array_location, 1, sizeof(int)); */
+    *slot_array_pointer = (int *)1;
   } else {
     /* All memory blocks are taken */
     return NULL;
   }
 
   /* Calculate address from the available slot index */
-  address = (unsigned long)get_MEM_BLOCK_START() + (slot * sizeof(task_t));
-  return (void *)address;
+  pointer = (unsigned long)get_MEM_BLOCK_START() + memory_management_offset + (slot_array_index * sizeof(task_t));
+  return (void *)pointer;
 }
 
 void task_free(void *ptr) {
-  unsigned long slot;
+  int *slot_pointer;
+  int slot_location;
+  unsigned long slot_index;
 
   /* Ignore if the pointer is null */
   if (ptr == NULL) return;
 
   /* Calculate to which slot this pointer belongs */
-  slot = ((unsigned long)ptr - (unsigned long)get_MEM_BLOCK_START()) / sizeof(task_t);
+  slot_index = ((unsigned long)ptr - (unsigned long)get_MEM_BLOCK_START() - memory_management_offset) / sizeof(task_t);
 
   /* Also ignore if slot is out of bounds */
-  if (slot < 0 || slot >= MAX_TASK_COUNT) return;
-
-  memory_slots[slot] = (int *)0;
+  if (slot_index < 0 || slot_index >= MAX_TASK_COUNT) return;
+  
+  slot_location = (unsigned long)get_MEM_BLOCK_START() + sizeof(int) * slot_index;
+  slot_pointer = (int *)slot_location;
+  *slot_pointer = (int *)0;
 }
