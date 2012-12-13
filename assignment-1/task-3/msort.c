84f53c7040f49bbaf8776182d180471d66d302b2
Bas van den Brink <brink.basvanden@gmail.com>
2012-12-13 13:11:10 +0100

    Fretje zou nu moeten werken.

--- assignment-1/task-3/msort.c
+++ assignment-1/task-3/msort.c
@@ -1,4 +1,5 @@
 #include "as1_t2.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
@@ -7,88 +8,99 @@ typedef struct {
   int count;
 } data;
 
-/* Copy a part of an array to an new array, beginning with start and ending with start + count - 1. */
-task_t** splitTasks(task_t** tasks, int start, int count){  
-  int i;
-
-  // Allocate memory or return NULL.
-  task_t** result = (task_t**) malloc(sizeof(task_t*) * count);
-  if (NULL == result) return NULL;
-
-  // Generate sublist
-  for (i = 0; i < count; i++) {
-    result[i] = tasks[start + i];       
-  }
-  return result;
-}
-
-/* We need an other function that is taking one parameter. ThreadSort just calls msort */
 void *threadSort(void *args) {
-	data* data = args;
-	msort(data->tasks, data->count);
-	return;
+  data* data = args;
+  msort(data->tasks, data->count);
 }
 
 /* Merge the splitted arays, namely tasksleft and tasksright, and put the result in tasks. */
-void merge(task_t** tasks, task_t** tasksleft, int size_left,task_t** tasksright, int size_right){
+void merge(task_t** tasks, int size_left, int size_right) {
+  task_t** taskscopy;
+  taskscopy = malloc(sizeof(task_t) * (size_left + size_right));
+
+  // Check if allocation was successful
+  if(taskscopy == NULL) {
+    printf("Out of memory.");
+    exit(EXIT_FAILURE);
+  }
+
+  // Create a copy of the tasks
+  int i;
+  for (i = 0; i < size_left + size_right; i++) {
+    taskscopy[i] = tasks[i];       
+  }
+
   // Initialize index pointers for tasks, tasksleft and tasksright
   int p, p1, p2;
   p = p1 = p2 = 0;
 
   while (p1 < size_left && p2 < size_right) { 
     // Check whether the index pointers p1 and p2 are stil large enough
-    if (tasksleft[p1]->id < tasksright[p2]->id) { 
+    if (taskscopy[p1]->id < taskscopy[size_left + p2]->id) { 
       // The smallest element in left is smaller than the smallest element in right
-      tasks[p++] = tasksleft[p1++];
+      tasks[p++] = taskscopy[p1++];
     } else {
       // The smallest element in right is smaller than the smallest element in left
-      tasks[p++] = tasksright[p2++];
+      tasks[p++] = taskscopy[size_left + p2++];
     }
   }
   while (p1 < size_left) { 
     // The right one is empty; now add everything that is left in the left one
-    tasks[p++] = tasksleft[p1++];
+    tasks[p++] = taskscopy[p1++];
   }
 
   while (p2 < size_right) { 
     // The left one is empty; now add everything that is left in the right one
-    tasks[p++] = tasksright[p2++]; 
-  }        
+    tasks[p++] = taskscopy[size_left + p2++]; 
+  }
+
+  free(taskscopy);       
 }
 
+int counter = 1;
+int max_threads = 4;
+
 /* Recursive way to do mergesort on tasks with length count with the use of mutiple threads. */
-void msort(task_t** tasks, int count){
+void msort(task_t** tasks, int count) {
   // Base case: 0 or 1 element are already 'sorted'
   if (count <= 1){
-    return;	
+    return; 
   }
   
   //create data struct for left array
   data* data_l;
   data_l = malloc(sizeof(data));
+  data_l->tasks = tasks;
   
   //create data struct for right array
   data* data_r;
-  data_l = malloc(sizeof(data));
-
+  data_r = malloc(sizeof(data));
+  
   // Divide input list length in two parts
   data_l->count = count / 2;
   data_r->count = count - data_l->count;
   
-  // Make sublist with these sizes
-  data_l->tasks = splitTasks(tasks, 0, data_l->count);
-  data_r->tasks = splitTasks(tasks, data_l->count, data_r->count);
+  data_r->tasks = tasks + data_l->count;
   
-  //call msort via threadSort in a recursive way on data_l
-  pthread_t merge_sort_l;  
-  pthread_create(&merge_sort_l, NULL, threadSort, data_l);
+  pthread_t merge_sort_l;
+  if (counter < max_threads && pthread_create(&merge_sort_l, NULL, threadSort, data_l) == 0){
+    counter ++;
+    //call msort via threadSort in a recursive way on data_l
+    threadSort(data_r);
+	//we cannot merge before the other thread is done
+    if (pthread_join(merge_sort_l, NULL) != 0){
+	  printf("error joining thread");
+      exit(-1);
+	}
+	counter --;
+  } else {
+    threadSort(data_l);
+	threadSort(data_r);
+  }
+
+  free(data_l);
+  free(data_r);
   
-  //while merge_sort_l is running, sort the right array in the current thread
-  threadSort(data_r);
-	
-  //we cannot merge before the other thread is done
-  pthread_join(merge_sort_l, NULL);
-	
   // Merge the sorted sublists together
-  merge(tasks, data_l->tasks, data_l->count, data_r->tasks, data_r->count);
+  merge(tasks, data_l->count, data_r->count);
 }
