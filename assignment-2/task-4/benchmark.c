57c7db5a2bca5dcf25cf37b9f5cef79bec1699f2
Bas van den Brink <brink.basvanden@gmail.com>
2013-01-08 17:45:43 +0100

    Added documentation.

--- assignment-2/task-4/benchmark.c
+++ assignment-2/task-4/benchmark.c
@@ -7,56 +7,27 @@
 
 #define TASKS_COUNT 100000
 
+/*This method calculates the time elapsed given the startTime and stopTime as a timeval struct. This method return the time elapsed in microseconds as a long.*/
 long calculateTimeElapsed (struct timeval startTime, struct timeval stopTime){
 	return (stopTime.tv_sec - startTime.tv_sec) * 1000000 + (stopTime.tv_usec - startTime.tv_usec);
 }
 
-void justATest (long useconds) {
-	struct timeval startTime;
-	struct timeval stopTime;
-	long startTimeus;
-	long stopTimeus;
-	long timeElapsed;
-
-	printf("justATest with %d microseconds of waiting\n", useconds);
-
-	if (gettimeofday(&startTime, NULL) != 0) {
-		printf("Error getting time of day\n");
-		exit(EXIT_FAILURE);
-	}
-
-	usleep(useconds);
-
-	if(gettimeofday(&stopTime, NULL) != 0) {
-		printf("Error getting time of day\n");
-		exit(EXIT_FAILURE);
-	}
-
-	startTimeus = startTime.tv_sec * 1000000 + startTime.tv_usec;
-
-	printf("startTime in us: %d\n", startTimeus);
-
-	stopTimeus = stopTime.tv_sec * 1000000 + stopTime.tv_usec;
-
-	printf("stopTime in us: %d\n", stopTimeus);
-
-	timeElapsed = calculateTimeElapsed (startTime, stopTime);
-
-	printf("Time elapsed: %d microseconds\n", timeElapsed);
-}
-
+/*This method benchmarks malloc by allocating TASK_COUNT tasks and then deallocating them. It returns the time malloc took in microseconds.*/
 long mallocBenchmark (void) {
+	/*Array with pointers to tasks.*/
 	task_t *tasks[TASKS_COUNT];
 	struct timeval startTime;
 	struct timeval stopTime;
 	int i;
 	long timeElapsed;
 
+	/*Get the current time in microseconds.*/
 	if (gettimeofday(&startTime, NULL) != 0) {
 		printf("Error getting time of day\n");
 		exit(EXIT_FAILURE);
 	}
 
+	/*Allocate TASK_COUNT tasks using malloc and put the pointers in tasks.*/
 	for (i = 0; i < TASKS_COUNT; i++){
 		tasks[i] = malloc (sizeof (task_t));
 		if(tasks[i] == NULL) {
@@ -65,41 +36,50 @@ long mallocBenchmark (void) {
 		}
 	}
 
+	/*Deallocate the tasks using malloc.*/
 	for (i = 0; i < TASKS_COUNT; i++) {
 		free (tasks[i]);
 	}
 
+	/*Get the current time in microseconds.*/
 	if(gettimeofday(&stopTime, NULL) != 0) {
 		printf("Error getting time of day\n");
 		exit(EXIT_FAILURE);
 	}
 
+	/*Calculated how long it took.*/
 	timeElapsed = calculateTimeElapsed(startTime, stopTime);
 
 	return timeElapsed;
 
 }
 
+/*This method benchmarks task_alloc by allocating TASK_COUNT tasks and then deallocating them. It returns the time task_alloc took in microseconds.*/
 long task_allocBenchmark (void) {
+	/*Array with pointers to tasks.*/
 	task_t *tasks[TASKS_COUNT];
 	struct timeval startTime;
 	struct timeval stopTime;
 	int i;
 	long timeElapsed;
 
+	/*Get the current time in microseconds.*/
 	if (gettimeofday(&startTime, NULL) != 0) {
 		printf("Error getting time of day\n");
 		exit(EXIT_FAILURE);
 	}
 	
+	/*Allocate TASK_COUNT tasks using task_alloc.*/
 	for (i = 0; i < TASKS_COUNT; i++){
 		tasks[i] = task_alloc ();
 	}
 
+	/*Deallocate the tasks using task_alloc.*/
 	for (i = 0; i < TASKS_COUNT; i++) {
 		task_free (tasks[i]);
 	}
 
+	/*Get the current time in microseconds.*/
 	if(gettimeofday(&stopTime, NULL) != 0) {
 		printf("Error getting time of day\n");
 		exit(EXIT_FAILURE);
@@ -112,11 +92,14 @@ long task_allocBenchmark (void) {
 
 int main(int argc, char *argv[]){
 	long mallocResult, task_allocResult;
+
+	/*Benchmark malloc.*/
+	printf("Benchmarking malloc by allocating and deallocating %d tasks\n", TASKS_COUNT);
 	mallocResult = mallocBenchmark ();
-	task_allocResult = task_allocBenchmark();
-
 	printf("malloc took %d microseconds\n", mallocResult);
-	printf("task_alloc took %d miliseconds\n", task_allocResult);
 
-/*	justATest(2050000);*/
+	/*Benchmark task_alloc.*/
+	printf("Benchmarking task_alloc by allocating and deallocating %d tasks\n", TASKS_COUNT);
+	task_allocResult = task_allocBenchmark();
+	printf("task_alloc took %d microseconds\n", task_allocResult);
 }
