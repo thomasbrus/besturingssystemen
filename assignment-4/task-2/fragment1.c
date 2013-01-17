1dd4a7922a05728c3868193de171315971d41c47
wietzuh+git@gmail.com <wietzuh@gmail.com>
2013-01-17 12:04:54 +0100

    Updated comments

--- assignment-4/task-2/fragment1.c
+++ assignment-4/task-2/fragment1.c
@@ -3,7 +3,7 @@
 
 int main( int ac, char** av )
 {
-    char buf[90];
+    char buf[10];
     if( ac > 1 && strlen(av[1]) <= 90){ /* check whether string is not too long */
         strcpy( buf, av[1] );        
     }
--- assignment-4/task-2/fragment3.c
+++ assignment-4/task-2/fragment3.c
@@ -3,9 +3,11 @@
 
 int main( int ac, char** av )
 {
-    int numBytes = atoi( av[1] );
-    if( numBytes > 100 || numBytes <= 0) { /*check whether numBytes is not too big, and not invalid*/
-        return -1;
-    }    
-    int* something = (int*)malloc( numBytes );
+	if(ac == 2){ /*check whether parameter is given*/
+	    int numBytes = atoi( av[1] );
+	    if( numBytes > 100 || numBytes <= 0) { /*check whether numBytes is not too big, and not invalid*/
+	        return -1;
+	    }    
+	    int* something = (int*)malloc( numBytes );
+	}
 }
