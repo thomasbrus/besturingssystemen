441c72854e40aeaef868b05546198afe3d7863b2
tcoenraad <twan@coenraad.at>
2013-01-21 15:18:46 +0100

    Some magic cleaning occurred

--- assignment-4/task-3/secret_string.c
+++ assignment-4/task-3/secret_string.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-int main( int ac, char** av )
-{	
-	char leeg[1];
-	leeg[0] = 0;
+int main(int ac, char** av) {
 	int i = 0xdeadbeef;
-	char *string;
-	string = (char *) &i;
-	printf("%s%s\r\n","jasdoiwueo2u34eueaoirusodifuosdign8o5j039485jf043895jf30589fj450g8j3580fvj305f89k345034ugidfgjdhkdkfjggfjdfg", string);
+	char *exploit = (char*) &i;
+	printf("%s%s\r\n","AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", exploit);
+
 	return 0;
 }
-
