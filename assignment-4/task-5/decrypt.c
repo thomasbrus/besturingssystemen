14172d6aea2d3fb238a00c80fc1ccb8971c48282
Thomas Brus <thomas.brus@me.com>
2013-01-28 18:16:26 +0100

    Free char buffers each time (fixes segfault)

--- assignment-4/task-5/decrypt.c
+++ assignment-4/task-5/decrypt.c
@@ -5,6 +5,12 @@
 
 static int shift[] = { 2, 15, 14, 18, 15, 9, 25, 11, 19, 7, 12, 25, 24, 2, 25, 19, 9, 21, 3, 17, 22, 9, 8, 12, 9, 3, 19, 5, 13, 1, 6, 19, 18, 22, 19, 13, 3, 15, 23, 11, 16, 3, 2, 6, 3, 23, 13, 25, 7, 21, 0, 13, 12, 16, 13, 7, 23, 9, 17, 5, 10, 23, 22, 0, 23, 17, 7, 19, 1, 15, 20, 7, 6, 10, 7, 1, 17, 3, 11, 25, 4, 17, 16, 20, 17, 11, 1, 13, 21, 9, 14, 1, 0, 4, 1, 21, 11, 23, 5, 19, 24, 11, 10, 14, 11, 5, 21, 7, 15, 3, 8, 21, 20, 24, 21, 15, 5, 17, 25, 13, 18, 5, 4, 8, 5, 25, 15, 1, 9, 23 };
 
+int prompt(char *message, char **output) {
+  size_t size = 0;
+  printf("%s", message);
+  return getline(output, &size, stdin);
+}
+
 char *decrypt(char *input) {
   int i;
   char *output = malloc(strlen(input) + 1);
@@ -23,19 +29,18 @@ char *decrypt(char *input) {
 }
 
 int main(int argc, char *argv[]) {
-  size_t size;
-  char *encrypted_text;
-  char *decrypted_text;
-
   while (1) {
-    printf("%s", "Enter text to decrypt: ");
-    if (getline(&encrypted_text, &size, stdin) == -1) break;
+    char *encrypted_text = NULL;
+    char *decrypted_text = NULL;
+  
+    if (prompt("Enter text to decrypt: ", &encrypted_text) == -1) break;
+
     decrypted_text = decrypt(encrypted_text);
     printf("%s", decrypted_text);
-  }
 
-  free(encrypted_text);
-  free(decrypted_text);
+    free(encrypted_text);
+    free(decrypted_text);
+  }
   
   return 0;
 }
